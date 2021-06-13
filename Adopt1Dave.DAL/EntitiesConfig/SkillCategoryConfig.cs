1f36d4f3da54977ee61851df5bfbff5ddc378503
isaskou <isabel.skou@yahoo.fr>
2021-06-13 11:53:14 +0200

    add EntitiesConfig and Entity Framework

---
+++ Adopt1Dave.DAL/EntitiesConfig/SkillCategoryConfig.cs
@@ -0,0 +1,28 @@
+﻿using Adopt1Dave.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt1Dave.DAL.EntitiesConfig
+{
+    class SkillCategoryConfig : IEntityTypeConfiguration<SkillCategory>
+    {
+        public void Configure(EntityTypeBuilder<SkillCategory> builder)
+        {
+            builder.ToTable(nameof(SkillCategory));
+
+            builder.HasKey(sc => sc.SkillCategoryId);
+
+            builder.Property(sc => sc.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasIndex(sc => sc.Name)
+                .IsUnique();
+        }
+    }
+}
---
+++ Adopt1Dave.DAL/EntitiesConfig/SkillConfig.cs
@@ -0,0 +1,31 @@
+﻿using Adopt1Dave.DAL.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adopt1Dave.DAL.EntitiesConfig
+{
+    public class SkillConfig : IEntityTypeConfiguration<Skill>
+    {
+        public void Configure(EntityTypeBuilder<Skill> builder)
+        {
+            builder.ToTable(nameof(Skill));
+
+            builder.HasKey(s => s.SkillId);
+
+            builder.Property(s => s.Name)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasOne(s => s.SkillCategory)
+                .WithMany(sc => sc.Skills);
+
+            builder.HasIndex(s => s.Name)
+                .IsUnique();
+        }
+    }
+}
